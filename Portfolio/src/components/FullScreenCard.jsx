ef7ebdbb32733652b1f9b5f039d6ae784b96eeb1
Marcel Peyton <marcel-p@outlook.com>
2025-07-09 14:55:35 -0400

    added contact me

--- Portfolio/src/components/FullScreenCard.jsx
+++ Portfolio/src/components/FullScreenCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/FullScreenCard.css";
 import proPic from './profilePic.jpg';
 
@@ -8,7 +8,19 @@ const FullScreenCard = () => {
       <h3>
         About Me ~ Marcel Peyton 
       </h3>
-      <img class="profilePic" src={proPic} alt="Profile Picture" ></img>
+      <div class = "twobox">
+        <img class="profilePic" src={proPic} alt="Profile Picture" ></img>
+        {}
+            <div style= {{paddingTop: "20px", backgroundColor: "#8BCBF1"}}>
+    <div className="contact-page" style= {{ paddingTop: "50px", backgroundColor: "#CCEBF9", marginTop: "100px"}}>
+      <h1 className="contact-title"  style= {{ fontSize: "20px"}}>Contact Me</h1>
+      <p>Email: marcel-p@outlook.com</p>
+      <p>Phone: 410-474-6080</p>
+      
+    </div>
+    </div>
+        {}
+      </div>
       <p>
       I'm a Computer Engineer with a passion for software development, problem-solving, and continuous learning. A University of Maryland graduate, I'm always looking for new challenges to grow and make an impact. With a strong work ethic and adaptability, I thrive in both independent and team settings. Continue to check out my page and see a sneak peek of what I bring to the table.
       <br></br>
